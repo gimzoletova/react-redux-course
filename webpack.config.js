ce615a574e8bf6710ddb8434de842a74b2632fd6
gimzoletova <tzurik@gmail.com>
2019-05-21 20:36:50 +0300

    use env_vars instead of strings for firebase keys

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
-
+const webpack = require('webpack');
 const path = require('path'); //for joining the path elements
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'developement';
+if (process.env.NODE_ENV === 'developement') {
+    require('dotenv').config({path: '.env.developement'})//creates env_vars for firebase from the file that keeps them: .env.developement. 
+}//                                                         can do the same for test and prod when making different db's for them
 
 module.exports = (env) => {
     const isProduction = env === 'production';
@@ -27,7 +32,18 @@ module.exports = (env) => {
                 })
             }]
         },
-        plugins: [new ExtractTextPlugin('styles.css')],
+        plugins: [
+            new ExtractTextPlugin('styles.css'),
+            new webpack.DefinePlugin({
+                'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+                'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+                'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+                'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+                'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+                'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+                'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID)
+            })
+        ],
         devtool: isProduction ? 'source-map' : 'inline-source-map', // for debug, we get the actual src files we wrote and not the bundle file on comments & errors in the browser console
         devServer: {
             contentBase: path.join(__dirname, 'public'),
