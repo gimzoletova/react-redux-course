ff7cfe9233dbc67aa6fe56577697a13175e161d9
gimzoletova <tzurik@gmail.com>
2019-05-22 15:55:26 +0300

    tesdt

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
+    apiKey: 'AIzaSyBNuXU-zKbfHM95RBF2vI2jYPsvFWJoXX4',
+    authDomain: 'expensify-cc867.firebaseapp.com',
+    databaseURL: 'https://expensify-cc867.firebaseio.com',
+    projectId: 'expensify-cc867',
+    storageBucket: 'expensify-cc867.appspot.com',
+    messagingSenderId: '101392083316',
+    appId: '1:101392083316:web:3e1eaddca63bcfd'
   };
 
 firebase.initializeApp(firebaseConfig);
